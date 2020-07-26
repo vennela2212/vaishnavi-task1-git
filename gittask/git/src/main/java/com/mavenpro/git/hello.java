5084f251c450198374967707563b5f079f52cb60
vennela2212 <62596494+vennela2212@users.noreply.github.com>
2020-07-26 15:44:46 +0530

    Add files via upload

---
+++ gittask/git/src/main/java/com/mavenpro/git/hello.java
@@ -0,0 +1,13 @@
+package com.mavenpro.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class hello 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
